423839dbbc5323745b2e11d922a44b370015f649
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-02-15 23:42:04 +0700

    get data on table in database GO

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bwastartup/user"
 	"fmt"
 	"log"
 
@@ -11,12 +12,28 @@ import (
 func main() {
 	// Connecting Database Mysql
 	dsn := "user:password@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Connection to database is RUN")
+
+	var users []user.User
+	length := len(users)
+
+	fmt.Println(length)
+
+	db.Find(&users)
+
+	length = len(users)
+	fmt.Println(length)
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+		fmt.Println("=================")
+	}
 
 }
