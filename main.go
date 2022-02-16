4001ab88dd119258ab03ec556f0e8f0abaf70571
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-02-16 13:19:23 +0700

    test register user

--- main.go
+++ main.go
@@ -3,56 +3,25 @@ package main
 import (
 	"bwastartup/user"
 	"log"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
 	// Connecting Database Mysql
-	// dsn := "user:password@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// fmt.Println("Connection to database is RUN")
-
-	// var users []user.User
-	// length := len(users)
-
-	// fmt.Println(length)
-
-	// db.Find(&users)
-
-	// length = len(users)
-	// fmt.Println(length)
-
-	// for _, user := range users {
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// 	fmt.Println("=================")
-	// }
-
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run()
-
-}
-
-func handler(c *gin.Context) {
 	dsn := "user:password@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	var users []user.User
-	db.Find(&users)
 
-	c.JSON(http.StatusOK, users)
+	userRepository := user.NewRepository(db)
+	user := user.User{
+		Name: "Test simpan",
+	}
+
+	userRepository.Save(user)
 
 }
