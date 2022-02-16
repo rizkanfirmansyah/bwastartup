0f913bd41606b12d9e34aa44b671f5cf60161378
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-02-16 12:52:29 +0700

    query database to JSON handler

--- main.go
+++ main.go
@@ -2,38 +2,57 @@ package main
 
 import (
 	"bwastartup/user"
-	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
 	// Connecting Database Mysql
+	// dsn := "user:password@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+
+	// fmt.Println("Connection to database is RUN")
+
+	// var users []user.User
+	// length := len(users)
+
+	// fmt.Println(length)
+
+	// db.Find(&users)
+
+	// length = len(users)
+	// fmt.Println(length)
+
+	// for _, user := range users {
+	// 	fmt.Println(user.Name)
+	// 	fmt.Println(user.Email)
+	// 	fmt.Println("=================")
+	// }
+
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+
+}
+
+func handler(c *gin.Context) {
 	dsn := "user:password@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-
-	fmt.Println("Connection to database is RUN")
-
 	var users []user.User
-	length := len(users)
-
-	fmt.Println(length)
-
 	db.Find(&users)
 
-	length = len(users)
-	fmt.Println(length)
-
-	for _, user := range users {
-		fmt.Println(user.Name)
-		fmt.Println(user.Email)
-		fmt.Println("=================")
-	}
+	c.JSON(http.StatusOK, users)
 
 }
