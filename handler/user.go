e4b453d86e04868930ef3c9bbfd9cd5ecd50286c
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-02-18 09:36:00 +0700

    formatter JSON Response API

--- handler/user.go
+++ handler/user.go
@@ -25,13 +25,15 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, nil)
 	}
 
-	user, err := h.userService.RegisterUser(input)
+	newUser, err := h.userService.RegisterUser(input)
 
 	if err != nil {
 		c.JSON(http.StatusBadRequest, nil)
 	}
 
-	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", user)
+	formatter := user.FormatUser(newUser, "tokenjwt")
+
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", formatter)
 
 	c.JSON(http.StatusOK, response)
 
