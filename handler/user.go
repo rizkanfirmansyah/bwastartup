0105b230e51743b504de87c5cb7d334ee891d85c
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-02-18 09:27:22 +0700

    add  handler API Response

--- handler/user.go
+++ handler/user.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"bwastartup/helper"
 	"bwastartup/user"
 	"net/http"
 
@@ -30,6 +31,8 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, nil)
 	}
 
-	c.JSON(http.StatusOK, user)
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", user)
+
+	c.JSON(http.StatusOK, response)
 
 }
