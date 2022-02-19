44fcea7bace2826d4b45008e7329f352296d7777
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-02-19 10:29:08 +0700

    add func FetchUser

--- handler/user.go
+++ handler/user.go
@@ -78,3 +78,15 @@ func (h *userHandler) Login(c *gin.Context) {
 	c.JSON(http.StatusOK, response)
 
 }
+
+func (h *userHandler) FetchUser(c *gin.Context) {
+
+	currentUser := c.MustGet("currentUser").(user.User)
+
+	formatter := user.FormatUser(currentUser, "")
+
+	response := helper.APIResponse("Successfully fetch user data", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+
+}
