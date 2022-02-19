c2439d5ea224ef7cf199b89fac6b326ca957dfd2
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-02-19 20:01:31 +0700

    handler & Endpoint upload avatar

--- handler/user.go
+++ handler/user.go
@@ -124,6 +124,43 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 	// JWR (sementara hardcode, seakan-akan user yg login ID = 1)
 	// repo ambil data user yg ID = 1
 	// repo update data user simpan lokasi file
+
+	file, err := c.FormFile("avatar")
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIResponse("Failed to upload avatar image", http.StatusBadRequest, "error", data)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	path := "images/" + file.Filename
+
+	err = c.SaveUploadedFile(file, path)
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIResponse("Failed to upload avatar image", http.StatusBadRequest, "error", data)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	userID := 1
+
+	_, err = h.userService.SaveAvatar(userID, path)
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIResponse("Failed to upload avatar image", http.StatusBadRequest, "error", data)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	data := gin.H{"is_uploaded": true}
+	response := helper.APIResponse("Avatar succesfully uploaded", http.StatusOK, "success", data)
+
+	c.JSON(http.StatusOK, response)
+
 }
 
 func (h *userHandler) FetchUser(c *gin.Context) {
