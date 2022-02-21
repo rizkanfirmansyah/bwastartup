f44aa2206b4d032373c60b7edc0c0920a5187487
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-02-21 23:17:55 +0700

    add middleware on post avatar

--- handler/user.go
+++ handler/user.go
@@ -146,7 +146,8 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 		return
 	}
 
-	userID := 1
+	currentUser := c.MustGet("currentUser").(user.User)
+	userID := currentUser.ID
 
 	// path := "images/" + file.Filename
 	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
