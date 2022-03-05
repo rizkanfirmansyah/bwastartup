d27bb56692d268ef69dd04bfb1cf7e9d2a5ae680
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-03-05 20:23:04 +0700

    add new service

--- main.go
+++ main.go
@@ -28,24 +28,12 @@ func main() {
 	userRepository := user.NewRepository(db)
 	campaignRepository := campaign.NewRepository(db)
 
-	campaigns, err := campaignRepository.FindByUserID(1)
-
-	fmt.Println("debug")
-	fmt.Println("debug")
-	fmt.Println("debug")
-	fmt.Println(len(campaigns))
-
-	for _, campaign := range campaigns {
-		fmt.Println(campaign.Name)
-		if len(campaign.CampaignImages) > 0 {
-			fmt.Println(campaign.CampaignImages[0].FileName)
-		}
-	}
-
 	userService := user.NewService(userRepository)
+	campaignService := campaign.NewService(campaignRepository)
 	authService := auth.NewService()
 
-	userService.SaveAvatar(1, "image/1-profile.png")
+	campaigns, _ := campaignService.FindCampaigns(9)
+	fmt.Println(len(campaigns))
 
 	userHandler := handler.NewUserHandler(userService, authService)
 
