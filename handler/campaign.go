9f3eca878a50cefa182c48c9d4c22ae95d4eb141
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-03-07 17:58:23 +0700

    add handler

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,7 +1,37 @@
 package handler
 
+import (
+	"bwastartup/campaign"
+	"bwastartup/helper"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
 // Tangkap parameter di handler
 // handler ke service
 // service yg menentukan repository mna yang di call
 // repository : FindAll, FindByUserID
 // db
+
+type campaignHandler struct {
+	service campaign.Service
+}
+
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+func (h *campaignHandler) GetCampaigns(c *gin.Context) {
+	userID, _ := strconv.Atoi(c.Query("user_id"))
+
+	campaigns, err := h.service.GetCampaigns(userID)
+	if err != nil {
+		response := helper.APIResponse("Error to get campaigns", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	response := helper.APIResponse("List of Campaigns", http.StatusOK, "success", campaigns)
+	c.JSON(http.StatusOK, response)
+}
