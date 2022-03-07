82ae0c6187e22789a85b35e14d6d2a538f6f2a3e
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-03-07 18:49:24 +0700

    add formatter campaign

--- handler/campaign.go
+++ handler/campaign.go
@@ -32,6 +32,6 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, response)
 		return
 	}
-	response := helper.APIResponse("List of Campaigns", http.StatusOK, "success", campaigns)
+	response := helper.APIResponse("List of Campaigns", http.StatusOK, "success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, response)
 }
