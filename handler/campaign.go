6ccb1c5b0370ac2c2f4fc0b84452057bc8e3d239
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-03-16 17:24:12 +0700

    add camapign by ID

--- handler/campaign.go
+++ handler/campaign.go
@@ -40,4 +40,23 @@ func (h *campaignHandler) GetCampaign(c *gin.Context) {
 	// handler : mapping id yg di url ke struct input =>service, call formatter
 	// service : inputnya struct input => data id dari url, manggil repo
 	// repository : get campaign by Id
+
+	var input campaign.GetCampaignDetailInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get detail of campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	campaign, err := h.service.GetCampaignByID(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get detail of campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Campaign detail", http.StatusOK, "success", campaign)
+	c.JSON(http.StatusOK, response)
 }
