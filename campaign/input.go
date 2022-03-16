a73e5ae8e5d0cfe048be3654d3bf53d37d6fdbbb
rizkanfirmansyah <riezkanaprianda@gmail.com>
2022-03-16 19:13:50 +0700

    new service create campaign

--- campaign/input.go
+++ campaign/input.go
@@ -1,5 +1,16 @@
 package campaign
 
+import "bwastartup/user"
+
 type GetCampaignDetailInput struct {
 	ID int `uri:"id" binding:"required"`
+}
+
+type CreateCampaignInput struct {
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	Description      string `json:"description"`
+	GoalAmount       int    `json:"goal_amount"`
+	Perks            string `json:"perks"`
+	User             user.User
 }
--- campaign/service.go
+++ campaign/service.go
@@ -3,6 +3,7 @@ package campaign
 type Service interface {
 	GetCampaigns(userID int) ([]Campaign, error)
 	GetCampaignByID(input GetCampaignDetailInput) (Campaign, error)
+	CreateCampaign(input CreateCampaignInput) (Campaign, error)
 }
 
 type service struct {
@@ -39,4 +40,23 @@ func (s *service) GetCampaignByID(input GetCampaignDetailInput) (Campaign, error
 	}
 
 	return campaign, nil
+}
+
+func (s *service) CreateCampaign(input CreateCampaignInput) (Campaign, error) {
+	campaign := Campaign{}
+
+	campaign.Name = input.Name
+	campaign.ShortDescription = input.ShortDescription
+	campaign.Description = input.Description
+	campaign.GoalAmount = input.GoalAmount
+	campaign.Perks = input.Perks
+	campaign.UserID = input.User.ID
+
+	newCampaign, err := s.repository.Save(campaign)
+
+	if err != nil {
+		return newCampaign, err
+	}
+
+	return newCampaign, nil
 }
